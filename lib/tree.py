392fdf885db908eb52824c69d22b9cc269852a03
Scott Schwab <scottschwab86@gmail.com>
2023-08-31 20:23:40 -0500

    Complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,44 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+
+      if node['id'] == id:
+        return node
+      else:
+        # nodes_to_visit = (node['children'] + nodes_to_visit)
+        nodes_to_visit = nodes_to_visit + node['children']
+
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def depth_first_traversal(node, result = []):
+#   # visit each node (add it to the list of results)
+#   result.append(node['value'])
+#   for child in node['children']:
+#     # visit each child node
+#     depth_first_traversal(child, result)
+
+#   return result
